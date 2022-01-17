3c818850306bd18b503d2dac881fb6aec9fceb91
Saurabh Sheshrao Khandagale <59130610+Khandagale-Saurabh@users.noreply.github.com>
2022-01-18 01:24:40 +0530

    Update Linkedlist.java

--- Linkedlist.java
+++ Linkedlist.java
@@ -1,13 +1,22 @@
 * always remember to reduce size ,or size++
 * allocate memory, set newnode.next=null,set value of newnode,
-remove:
-temp.next=temp.next.next 
+
+Q] remove:
+        temp.next=temp.next.next 
 
 
 
-rever using swap: we can swap using getdata()
+Q] rever using swap: we can swap using getdata()
         lf=getData(left)
         rtgetData(right);
         swap(lf,rt)
   
-
+Q] Merge SortedList
+        addLast()===> 
+        size++
+                
+Q]Remove duplicate:
+     getFirst();
+   rd=  removeFirst();
+     if(size== 0|| reasultList.tail.data!=getFirst())
+             resultList.addLast(rd);
