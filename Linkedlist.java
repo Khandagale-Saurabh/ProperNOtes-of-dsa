6fb1b9c085e739270246061b9e509c0ff5f2980e
Saurabh Sheshrao Khandagale <59130610+Khandagale-Saurabh@users.noreply.github.com>
2022-01-17 23:56:01 +0530

    Update Linkedlist.java

--- Linkedlist.java
+++ Linkedlist.java
@@ -1,2 +1,13 @@
+always remember to reduce size
+
 remove:
 temp.next=temp.next.next 
+
+
+
+rever using swap: we can swap using getdata()
+        lf=getData(left)
+        rtgetData(right);
+        swap(lf,rt)
+  
+
